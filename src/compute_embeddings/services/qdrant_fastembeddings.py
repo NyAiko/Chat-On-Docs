937cb667a06ea5cc720eb8a451e1a94a47fcf23c
NyAiko <rindranyaiko@gmail.com>
2024-04-10 19:46:29 +0300

    Changes to be committed: 	modified:   requirements.txt 	modified:   src/compute_embeddings/services/qdrant_fastembeddings.py

--- src/compute_embeddings/services/qdrant_fastembeddings.py
+++ src/compute_embeddings/services/qdrant_fastembeddings.py
@@ -2,6 +2,6 @@ import fastembed
 from fastembed import TextEmbedding
 embed = TextEmbedding(model_name='BAAI/bge-small-en-v1.5')
 
-def get_text_embeddings(text:str):
+async def get_text_embeddings(text:str):
   embeddings = list(embed.embed('Hi'))[0].astype('float16').tolist() #Vector embeddings output
   return embeddings

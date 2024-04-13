de55c19384f158c198475705d3e1ca658a04c2d0
NyAiko <rindranyaiko@gmail.com>
2024-04-13 15:23:37 +0300

    modified:   src/compute_embeddings/services/compute_embeddings.py

--- src/compute_embeddings/services/compute_embeddings.py
+++ src/compute_embeddings/services/compute_embeddings.py
@@ -1,11 +1,51 @@
-from fastembed.embedding import TextEmbedding
-from typing import List
+import os
+import json
 import numpy as np
-from joblib import load
+import onnxruntime
+from transformers import BertTokenizer, BertConfig
+from dotenv import load_dotenv
 
-emb = TextEmbedding(model_name = 'BAAI/bge-small-en-v1.5')
+# Load environment variables from .env file
+load_dotenv()
 
-def get_text_embeddings(text:List[str]):
-  embeddings = np.array(list(emb.embed(text)))
-  return embeddings[0].tolist()
+# Define paths relative to the current script file
+model_path = os.path.join(os.path.dirname(__file__),'model_optimized.onnx')
+config_path = os.path.join(os.path.dirname(__file__), "config.json")
+ort_config_path = os.path.join(os.path.dirname(__file__), "ort_config.json")
+special_tokens_path = os.path.join(os.path.dirname(__file__), "special_tokens_map.json")
+tokenizer_path = os.path.join(os.path.dirname(__file__), "tokenizer.json")
+tokenizer_config_path = os.path.join(os.path.dirname(__file__), "tokenizer_config.json")
+vocab_path = os.path.join(os.path.dirname(__file__), "vocab.txt")
+
+# Load ONNX model
+with open(model_path, 'rb') as f:
+    ort_session = onnxruntime.InferenceSession(f.read())
+
+# Load config
+with open(config_path, "r") as f:
+    config = json.load(f)
+
+# Load ORT config
+with open(ort_config_path, "r") as f:
+    ort_config = json.load(f)
+
+# Load special tokens
+with open(special_tokens_path, "r") as f:
+    special_tokens = json.load(f)
+
+# Load tokenizer
+tokenizer = BertTokenizer(tokenizer_file=tokenizer_path, vocab_file=vocab_path)
+tokenizer_config = BertConfig.from_pretrained(tokenizer_config_path)
+tokenizer_config.vocab_file = vocab_path
+
+
+async def get_text_embeddings(text):
+    # Tokenize the input text
+    inputs = tokenizer(text, return_tensors="np", padding=True, truncation=True)
+    # Prepare inputs for ONNX runtime
+    ort_inputs = {input_meta.name: np.atleast_2d(inputs[input_meta.name]) for input_meta in ort_session.get_inputs()}
+    # Run inference
+    outputs = ort_session.run(None, ort_inputs)
+    cls_embedding = outputs[0][:, 0, :]  # Extract embeddings for the [CLS] token
+    return cls_embedding.astype(np.float16)
 

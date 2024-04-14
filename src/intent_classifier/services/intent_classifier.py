7b7be060bc1c256d436abb84eb5295f7de5baad0
NyAiko <rindranyaiko@gmail.com>
2024-04-14 17:58:16 +0300

    modified:   .github/workflows/main.yml 	modified:   src/intent_classifier/services/intent_classifier.py

--- src/intent_classifier/services/intent_classifier.py
+++ src/intent_classifier/services/intent_classifier.py
@@ -3,7 +3,7 @@ from joblib import load
 emb_fn = embeddings_function.FastEmbeddingFunction()
 import numpy as np
 
-async def detect_intent(text:str):
+async def classify_intent(text:str):
     vec = await emb_fn(text)
     sgd = load('sgd.p')
     return sgd.predict([np.array(vec)])[0]
